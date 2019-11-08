0d9db9a5da603298b1faf2cf55bf53b81d99674f
TharinduWithanage <56493297+TharinduWithanage@users.noreply.github.com>
2019-11-08 06:54:28 +0530

    data_types

---
+++ SecondProgram.java
@@ -0,0 +1,18 @@
+public class SecondProgram{
+public static void main(String args[]){
+System.out.println("Tharindu Dulshan");
+System.out.println("Telijjawila");
+System.out.println("hungry");
+int numberOfStudents=230;
+System.out.println(numberOfStudents);
+long a=711565953L;
+System.out.println(a);
+double b=2399.4;
+System.out.println(b);
+String name="Niyomal";
+System.out.println(name);
+int ch='5';
+System.out.println(ch);
+System.out.println("Sri \' Lanka");
+}
+}
